be952e7f1141140a6ca62b4f5a6eab03e72c2438
Janit Garg <jangarg@BLR.ALLEGISINDIA.COM>
2024-08-13 13:05:07 +0530

    This component is created to understand LDS This is an important component, kindly review once This component uses LDS Review form This also uses SHOWTOAST to pop-up message once the operation is done.

---
+++ force-app/main/default/lwc/realTimeAccountComponentLDS/realTimeAccountComponentLDS.js
@@ -0,0 +1,32 @@
+import { LightningElement, api, wire} from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+const FIELDS = ['Account.Name', 'Account.Industry', 'Account.Phone'];
+
+export default class RealTimeAccountComponentLDS extends LightningElement {
+    @api recordId;
+    isEditFormVisible = false;
+
+    @wire(getRecord, {recordId: '$recordId', fields: FIELDS})
+    account;
+
+    handleEdit(){
+        this.isEditFormVisible = true;
+    }
+
+    handleCancel(){
+        this.isEditFormVisible = false;
+    }
+
+    handleSuccess(){
+        this.isEditFormVisible = false;
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Account Updated',
+                message: 'Account has been updated successfully',
+                variant: 'success'
+            })
+        );
+    }
+}
